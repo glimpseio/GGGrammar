a4601ca13c08c2cd00e030f25e7eb658fec20dba
marcprux <marc@glimpse.io>
2021-03-24 23:26:36 -0400

    build: package version update

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["GGSpec"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/glimpseio/BricBrac", .branch("main")),
+        .package(url: "https://github.com/glimpseio/BricBrac.git", .branch("main")),
     ],
     targets: [
         .target(
